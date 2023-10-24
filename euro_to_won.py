abf84a66208387a60b820cc4c765493e297b85be
onyourYujin <ys991120@gmail.com>
2023-10-24 16:21:42 +0200

    EURO to WON converter

---
+++ euro_to_won.py
@@ -0,0 +1,49 @@
+from tkinter import *
+import requests
+from bs4 import BeautifulSoup
+
+url ="https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%9C%A0%EB%A1%9C"
+res = requests.get(url)
+res.raise_for_status()
+soup = BeautifulSoup(res.text,"lxml")
+
+euro_naver_str = soup.find("div",attrs={"class":"price_info"}).find("strong").get_text()
+euro_naver_str = euro_naver_str.replace(",", "")  # str인 ","가 들어오면 int나 float로 변경 불가
+euro_naver = float(euro_naver_str)
+
+def money_convert():
+    euro = int(EURO_input.get())  # type : str => float으로 바꿔주기
+    money_calculate = euro * euro_naver
+    result.config(text=f"{money_calculate:.2f}")  # 숫자를 label에 넣으려면 str 형태로 바꿔야 함
+
+# 새로운 window 화면 설정
+window = Tk()
+window.title("EURO to WON converter")
+window.minsize(width=200, height=100)
+window.config(padx=20, pady=20)
+
+# Entry
+EURO_input = Entry(width=7)
+EURO_input.grid(column=1, row=0)
+
+
+# Labels
+euro = Label(text="EURO")
+euro.grid(column=2, row=0)
+
+equal = Label(text="is equal to")
+equal.grid(column=0, row=1)
+
+won = Label(text="WON")
+won.grid(column=2,row=1)
+
+result = Label(text="0")
+result.grid(column=1,row=1)
+
+
+# Buttons
+calculate_button = Button(text="Calculate", command=money_convert)  # 괄호 안에 함수를 넣을 때 괄호 빼고(클릭 했을 때만 메소드 호출)
+calculate_button.grid(column=1, row=2)
+
+
+window.mainloop()
